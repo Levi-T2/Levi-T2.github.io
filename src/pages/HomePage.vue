1f146b343d52e3cf3fcc64ccb0cf67e3c408996d
Levi <lttyler713@gmail.com>
2023-12-20 09:54:37 -0700

    Just need to add footer and skill imgs

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -15,7 +15,7 @@
         </div>
       </div>
       <div class="col-12 my-4">
-        <div class="col-6 px-3 d-flex justify-content-start flex-column ">
+        <div class="col-12 col-md-6 px-3 d-flex justify-content-start flex-column ">
           <h2 class="txt-gray">About Me</h2>
           <p class="mb-0 px-4 txt-dark-gray">
             I am very interested in learning more about the industry and learning new skills and technologies to be able
@@ -25,7 +25,46 @@
       </div>
       <div class="col-12 mt-3 project-slanted">
         <h2 class="p-3">Projects</h2>
-
+        <section class="row">
+          <div class="col-12 col-md-6">
+            <div class="d-flex flex-column align-items-center text-center p-1">
+              <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+                alt="Capstone Image" class="project-img">
+              <h4 class="mt-3 mb-0">Auto-Tunes</h4>
+              <p class="p-2">
+                As a team of four developers we were given two weeks to construct a UML, Figma Mock, and pitch for our
+                websites features.
+                For our website, we made a social media platform where users could share descriptions and images of their
+                modified cars and favorite other users cars.
+              </p>
+              <p></p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6">
+            <div class="d-flex flex-column align-items-center text-center p-1">
+              <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+                alt="Keepr Image" class="project-img">
+              <h4 class="mt-3 mb-0">Keepr</h4>
+              <p class="p-2">Keepr was our final project given to our cohort and it required us to utilize a Vue3
+                front-end with a C#
+                back-end for the project.
+                It required us to put 'Keeps' into 'Vaults' using a 'VaultKeep'; similar to Pinterest.</p>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div class="col-12 mt-4">
+        <section class="row">
+          <div class="col-12 col-md-6">
+            <h4 class="txt-gray">Get In Touch</h4>
+            <div class="px-5">
+              <p class="my-4 fs-3 txt-dark-gray"><i class="mdi mdi-phone"></i> 208-243-6963</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6">
+            <h4 class="txt-gray">Skills and Languages</h4>
+          </div>
+        </section>
       </div>
     </section>
   </div>
@@ -53,29 +92,36 @@ export default {
 }
 
 .project-slanted {
-  padding-bottom: 2.25rem;
+  padding-top: 3rem;
   background-color: rgb(51, 51, 51);
   color: white;
   clip-path: polygon(0% 0%,
-      100% calc(100% - 3rem),
+      100% 14.5%,
       100% 100%,
       0% 100%);
 }
 
+.project-img {
+  height: 14rem;
+  width: 24rem;
+  border-radius: 3.5px;
+  box-shadow: 8px 6.5px 0px 3px rgb(145, 115, 160);
+}
+
 .img-size {
-  height: 20rem;
-  width: 17.5rem;
+  height: 21rem;
+  width: 18.5rem;
   object-fit: cover;
   object-position: 0% 15%;
   border-radius: 3.5px;
-  box-shadow: 6.5px 6.5px 0px 3px rgb(145, 115, 160);
+  box-shadow: 0px 0px 0px 6.5px rgb(145, 115, 160);
 }
 
 .img-position {
   display: flex;
   justify-content: end;
   position: absolute;
-  padding-right: 8.5rem;
+  padding-right: 10.5rem;
   padding-top: 2.5rem;
 }
 
